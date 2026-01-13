9797751f1fe5e57a14a8f57c21b94856ef7b2862
kpole7 <kpole@slcj.uw.edu.pl>
2026-01-13 15:26:02 +0100

    Added serial port handling (it compiles but it has not been tested);

--- source/serial_communication.cpp
+++ source/serial_communication.cpp
@@ -26,7 +26,7 @@ void peripheralThread(void) {
 	PeripheralThreadTimeInMilliseconds = 0;
 	PeripheralThreadLoopStart = std::chrono::high_resolution_clock::now();
 	while(1){
-		{
+		{ // measure fixed time intervals
 			std::chrono::high_resolution_clock::time_point TimeNow;
 			std::chrono::milliseconds DurationTime;
 			PeripheralThreadTimeInMilliseconds += PERIPHERAL_THREAD_LOOP_DURATION;
@@ -39,14 +39,14 @@ void peripheralThread(void) {
 			}while(DurationTime.count() < PeripheralThreadTimeInMilliseconds);
 		}
 
-		{
+		{ // debugging: modify Modbus registers and display new values periodically
 			static unsigned TemporaryCounter;
 			static unsigned TemporaryDrift;
 			TemporaryCounter++;
 			TemporaryCounter &= 127;
 			if (0 == TemporaryCounter){
 
-			    TemporaryDrift += 23456.789;
+			    TemporaryDrift += 56.789;
 			    for (int Cup = 0; Cup < CUPS_NUMBER; Cup++){
 			    	for (int J=0; J < VALUES_PER_DISC; J++){
 			    		int TemporaryRegisterIndex = Cup*VALUES_PER_DISC + J;
@@ -76,3 +76,29 @@ void peripheralThread(void) {
 
 	}
 }
+
+// This function calculates crc16 of Modbus type for a given frame
+static uint16_t crc16( const uint8_t *Buffer, uint8_t Length ){
+	uint16_t crc;
+	uint8_t i;
+	uint8_t bit;
+
+	crc = 0xFFFFu;
+	for( i = 0; i < Length; i++ ){
+		crc ^= (uint16_t)Buffer[i];
+		for( bit = 0; bit < 8; bit++ ){
+			if((crc & 0x0001u) != 0u){
+				crc >>= 1;
+				crc ^= 0xA001u;
+			}
+			else{
+				crc >>= 1;
+			}
+		}
+	}
+	return crc;
+}
+
+
+
+
--- source/uart_ports.cpp
+++ source/uart_ports.cpp
@@ -1,6 +1,82 @@
 /// @file uart_ports.cpp
 
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string>
 #include "uart_ports.h"
 
+//.................................................................................................
+// Preprocessor directives
+//.................................................................................................
 
+// This constant determines Modbus speed; it is defined in termios.h
+#define MODBUS_RTU_HARDWARE_SPEED		B19200
+
+
+std::string PortName = "/dev/ttyUSB0";
+
+int SerialPortHandler;
+
+
+static int configureSerialPort(const char *DeviceName);
+
+
+//........................................................................................................
+// Local function definitions
+//........................................................................................................
+
+
+void openModbusPort( void ){
+	int Result;
+	static const char* PortNameCharPtr;
+
+	PortNameCharPtr = PortName.c_str();
+	Result = access(PortNameCharPtr, F_OK );
+	if(0 == Result){
+		SerialPortHandler = configureSerialPort( PortNameCharPtr );
+	}
+	if(-1 == SerialPortHandler){
+
+	}
+}
+
+
+// This function opens and configures a serial port
+static int configureSerialPort(const char *DeviceName){
+    int FileHandler;
+    struct termios PortSettings;
+
+    FileHandler = open(DeviceName, O_RDWR | O_NOCTTY | O_SYNC);
+    if (FileHandler == -1) {
+        return -1;
+    }
+
+    if (tcgetattr(FileHandler, &PortSettings) != 0) {
+        close(FileHandler);
+        return -1;
+    }
+
+    cfsetispeed(&PortSettings, MODBUS_RTU_HARDWARE_SPEED);
+    cfsetospeed(&PortSettings, MODBUS_RTU_HARDWARE_SPEED);
+
+    PortSettings.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON | IXOFF | IXANY);
+    PortSettings.c_oflag = 0;                            // Disable output processing
+    PortSettings.c_lflag = 0;                            // Mode 'raw'
+
+    PortSettings.c_cflag &= ~(CSIZE | CSTOPB | PARODD | CRTSCTS);
+    PortSettings.c_cflag |= (CS8 | PARENB | CREAD | CLOCAL);
+
+    PortSettings.c_cc[VMIN]  = 0;                        // Minimum number of bytes to read
+    PortSettings.c_cc[VTIME] = 0;                        // Timeout in tenth of a second
+
+    // apply the configuration
+    if (tcsetattr(FileHandler, TCSANOW, &PortSettings) != 0) {
+        close(FileHandler);
+        return -1;
+    }
+
+    return FileHandler;
+}
 
